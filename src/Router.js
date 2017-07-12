171f80771baf15d554eea001660b71bcef00a374
Seth Puckett <seth@Seths-MacBook-Pro.local>
2017-07-11 23:48:15 -0400

    Section 17 updates

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
-        <Router>
-            <Scene 
-                key='login'
-                component={LoginForm}
-                title="Please Login"
-            />
+        <Router sceneStyle={{ paddingTop: 65 }}>
+            <Scene key='login' component={LoginForm} title="Please Login" />
+            <Scene key='employeeList' component={EmployeeList} title="Employees" initial />
         </Router>
     );
 };
